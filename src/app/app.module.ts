ec9828d9896c76fb1dca14d57852759caa888387
KaZebra <104632198+KaZebra@users.noreply.github.com>
2023-06-01 21:53:22 +0200

    implemented address as interface and AddressList component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventComponent } from './event/event.component';
+import { AddressListComponent } from './address-list/address-list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     EventComponent,
+    AddressListComponent,
    
   ],
   imports: [
