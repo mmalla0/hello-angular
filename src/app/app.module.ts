9b287678fe05f76ca4cb00d41a324093c54d9b09
mmalla0 <mmalla-mirza@hs.bremen.de>
2023-06-05 22:35:03 +0200

    Clean up and init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EventComponent } from './event/event.component';
-
 
 @NgModule({
   declarations: [
     AppComponent,
-    EventComponent,
-   
   ],
   imports: [
     BrowserModule,
