ad5c0956351718d741f0958e13ee6db17f62d19e
KaZebra <104632198+KaZebra@users.noreply.github.com>
2023-06-10 13:08:25 +0200

    Formular-Template für die Buchung angelegt

--- src/app/order/order-form/order-form.component.ts
+++ src/app/order/order-form/order-form.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { Address } from 'src/app/shared/address';
+import { User } from 'src/app/shared/user';
 
 @Component({
   selector: 'app-order-form',
   templateUrl: './order-form.component.html',
-  styleUrls: ['./order-form.component.css']
+  styleUrls: ['./order-form.component.css'],
 })
 export class OrderFormComponent {
+  address: Address = {
+    id: null,
+    street: '',
+    houseNumber: 0,
+    postalCode: 0,
+    city: '',
+    country: '',
+    planet: '',
+  }
+  user: User = {
+    id: null,
+    firstName: '',
+    lastName: '',
+    addressId: this.address.id,                 //TODO: hier die addressID der eben angelegten Adresse holen
+    methodOfPayment: '',
+    cartId: null,
+    stockpileId: null,
+  }
 
 }
