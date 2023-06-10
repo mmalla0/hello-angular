5783233ce2097fbc756d40bbc861a7cd48b8ad2f
KaZebra <104632198+KaZebra@users.noreply.github.com>
2023-06-10 08:53:05 +0200

    Authentifizierungsfehler gelöst

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EventComponent } from './event/event.component';
 import { AddressListComponent } from './address-list/address-list.component';
 import { OrderFormComponent } from './order-form/order-form.component';
 import { AddressListItemComponent } from './address-list-item/address-list-item.component';
 import { CartComponent } from './cart/cart.component';
 import { CartItemComponent } from './cart-item/cart-item.component';
-
+import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ReactiveFormsModule }    from '@angular/forms';
@@ -17,7 +15,6 @@ import { ReactiveFormsModule }    from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
-    EventComponent,
     AddressListComponent,
     OrderFormComponent,
     AddressListItemComponent,
@@ -30,7 +27,8 @@ import { ReactiveFormsModule }    from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
