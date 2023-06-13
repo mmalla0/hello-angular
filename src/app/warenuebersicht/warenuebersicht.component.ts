4558a939c7d6148c18963df3b643a2c888ab741e
fekorte <fe.korte@web.de>
2023-06-14 01:30:57 +0200

    change color style

--- src/app/warenuebersicht/warenuebersicht.component.ts
+++ src/app/warenuebersicht/warenuebersicht.component.ts
@@ -14,25 +14,25 @@ export class WarenuebersichtComponent implements OnInit {
   searchQuery: string;
   priceRange: number;
   selectedCategory: string;
-  categories: string[] = ['Essen', 'Ausrüstung', 'Kleidung','Sonstiges','Kunst','Spielzeug','Gesundheit'];
+  categories: string[] = ['Food', 'Health', 'Fashion','Art','Equipment','Toys','Science', 'Other', 'No filter'];
 
 
   ngOnInit() {
 
     this.products = [
-      {id: 1, name: '2X2Re Stone', description: 'Mysteriöser Stein, suuuper wertvoll', price: 50000, bestBeforeDate: '02.01.2320', storeQuantity: 3, pictures: ['space_stone.jpeg'], categories: ['Sonstiges','Kunst']},
-      {id: 2, name: 'X-Wing', description: 'Relikt aus vergangenen Zeiten', price: 67658, bestBeforeDate: '-', storeQuantity: 30, pictures: ['x_wing.jpeg'], categories: ['Ausrüstung', 'Sonstiges']},
-      {id: 3, name: 'Spacetronics Starsuit', description: 'Very comfortable!', price: 200000, bestBeforeDate: '-', storeQuantity: 11, pictures: ['spacesuite.jpg'], categories: ['Ausrüstung', 'Kleidung']},
-      {id: 4, name: 'VirtuCare Pets - CatBot Edition', description: 'Macht sich perfekt auf der Raumschiff Armatur', price: 99.99, bestBeforeDate: '-', storeQuantity: 30, pictures: ['virtualpet_cat.jpg'], categories: ['Kunst', 'Spielzeug', 'Sonstiges']},
-      {id: 5, name: 'MediReady NanoBots', description: 'Wirkt immer', price:  5000.99, bestBeforeDate: '01.10.2300', storeQuantity: 30, pictures: ['healing_nanobots.jpg'], categories: ['Gesundheit']},
-      {id: 6, name: 'Gravity implant', description: 'Nie wieder schwindel', price:  500.99, bestBeforeDate: '20.08.2300', storeQuantity: 30, pictures: ['gravity_adjusting_implants.jpg'], categories: ['Ausrüstung']},
-      {id: 7, name: 'Repair set', description: 'Damit bekommst du alles heile', price:  3000.99, bestBeforeDate: '-', storeQuantity: 20, pictures: ['toolbox_spaceship_repair.jpg'], categories: ['Ausrüstung']},
-      {id: 8, name: 'Underwater breathing implant', description: 'Erkunde neue Welten', price:  360.01, bestBeforeDate: '03.20.2302', storeQuantity: 30, pictures: ['underwaterbreathing_implants.jpg'], categories: ['Ausrüstung']}
+      {id: 1, name: '2X2Re Stone', description: 'Mystery stone, veery precious', price: 50000, bestBeforeDate: '02.01.2320', storeQuantity: 3, pictures: ['space_stone.jpeg'], categories: ['Other','Art']},
+      {id: 2, name: 'X-Wing', description: 'Relic from past times...', price: 67658, bestBeforeDate: '-', storeQuantity: 30, pictures: ['x_wing.jpeg'], categories: ['Equipment', 'Other']},
+      {id: 3, name: 'Spacetronics Starsuit', description: 'Very comfortable!', price: 200000, bestBeforeDate: '-', storeQuantity: 11, pictures: ['spacesuite.jpg'], categories: ['Equipment', 'Fashion']},
+      {id: 4, name: 'VirtuCare Pets - CatBot Edition', description: 'The perfect companion', price: 99.99, bestBeforeDate: '-', storeQuantity: 30, pictures: ['virtualpet_cat.jpg'], categories: ['Art', 'Toys', 'Other']},
+      {id: 5, name: 'MediReady NanoBots', description: 'Works 100%', price:  5000.99, bestBeforeDate: '01.10.2300', storeQuantity: 30, pictures: ['healing_nanobots.jpg'], categories: ['Health']},
+      {id: 6, name: 'Gravity implant', description: 'No more dizziness', price:  500.99, bestBeforeDate: '20.08.2300', storeQuantity: 30, pictures: ['gravity_adjusting_implants.jpg'], categories: ['Equipment']},
+      {id: 7, name: 'Repair set', description: 'Repairs everything', price:  3000.99, bestBeforeDate: '-', storeQuantity: 20, pictures: ['toolbox_spaceship_repair.jpg'], categories: ['Equipment']},
+      {id: 8, name: 'Underwater breathing implant', description: 'Discover new worlds', price:  360.01, bestBeforeDate: '03.20.2302', storeQuantity: 30, pictures: ['underwaterbreathing_implants.jpg'], categories: ['Equipment']}
     ];
 
     this.filteredProducts = this.products;
     this.searchQuery = '';
-    this.priceRange = 0; // Set default price range value
+    this.priceRange = 200000; // Set default price range value
     this.selectedCategory = '';
 
     this.products.forEach(product => {
@@ -61,20 +61,16 @@ export class WarenuebersichtComponent implements OnInit {
   }
 
   onSearchProducts() {
-    this.filterProducts(); 
+    this.filteredProducts = this.products.filter(product => {
+      return this.searchQuery ? product.name.toLowerCase().includes(this.searchQuery.toLowerCase()) : true;
+    });
   }
 
   filterProducts() {
     this.filteredProducts = this.products.filter(product => {
       const isPriceInRange = product.price <= this.priceRange;
-      const isCategoryMatch = this.selectedCategory ? product.categories.includes(this.selectedCategory) : true;
-      const isSearchMatch = this.searchQuery ? product.name.toLowerCase().includes(this.searchQuery.toLowerCase()) : true;
-      return isPriceInRange && isCategoryMatch && isSearchMatch;
+      const isCategoryMatch = this.selectedCategory === 'No filter' ? true : product.categories.includes(this.selectedCategory);
+      return isPriceInRange && isCategoryMatch;
     });
   }
 }
-
-
-
-
-
