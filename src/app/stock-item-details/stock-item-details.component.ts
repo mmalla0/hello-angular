044eb3a172a738d6d96892cab091003cb021bf05
fekorte <fe.korte@web.de>
2023-07-06 22:29:12 +0200

    save state before i alter css again

--- src/app/stock-item-details/stock-item-details.component.ts
+++ src/app/stock-item-details/stock-item-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Item } from '../shared/item';
 import { FileUploadService } from '../services/file-upload-service/file-upload.service';
+import { CategoryService } from '../services/category-service/category.service';
+
 
 @Component({
   selector: 'app-stock-item-details',
@@ -18,21 +20,31 @@ export class StockItemDetailsComponent implements OnInit {
 
   categories: { category_name: string; selected: boolean }[] = [];
 
+
   ngOnInit() {
+    this.categoryService.getAllCategoryNames().subscribe({
+      next: categoryNames => {
+        this.categories = categoryNames.map(category => ({
+          category_name: category,
+          selected: false
+        }));
+      }
+    });
+  
     if (this.isEditing) {
       this.editedProduct = { ...this.product };
-      this.categories = this.editedProduct.categories.map(category => ({
-        category_name: category,
-        selected: true
-      }));
+      this.editedProduct.categories.forEach(category => {
+        const foundCategory = this.categories.find(c => c.category_name === category);
+        if (foundCategory) {
+          foundCategory.selected = true;
+        }
+      });
     } else {
-      this.editedProduct =  { item_ID: null, item_name:  '', item_description: null, item_price: null, stock: null, employee_id: null, best_before: '', item_imgpath: '', categories: []};
+      this.editedProduct = { item_ID: null, item_name: '', item_description: null, item_price: null, stock: null, employee_id: null, best_before: '', item_imgpath: '', categories: [] };
     }
   }
-
   
-
-  constructor(private fileUploadService: FileUploadService) {}
+  constructor(private fileUploadService: FileUploadService, private categoryService: CategoryService) {}
 
   save(): void {
     const editedProduct: Item = {
@@ -60,20 +72,19 @@ export class StockItemDetailsComponent implements OnInit {
   handleFileUpload(event: any) {
     this.file = event.target.files[0];
   }
-  
-  addCategory(): void {
-    this.categoriesToAdd.push('');
-  }
-
-  removeCategory(index: number): void {
-    this.categoriesToAdd.splice(index, 1);
-  }
 
   cancel() {
     this.cancelClick.emit();
   }
 
-  trackByIndex(index: number, item: any): number {
+  trackByIndex(index: number): number {
     return index;
+  }
+
+  updateCategorySelection(categoryName: string, newValue: boolean) {
+    const categoryObject = this.categories.find(c => c.category_name === categoryName);
+    if (categoryObject) {
+      categoryObject.selected = newValue;
+    }
   }
 }
