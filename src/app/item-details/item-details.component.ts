1efc24c068f4a010562f7a502520c24ae848cebe
AlgoArtee <laura.hodrea@gmail.com>
2023-07-08 02:34:59 +0200

    Item Details

--- src/app/item-details/item-details.component.ts
+++ src/app/item-details/item-details.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Item } from '../shared/item';
 
 @Component({
   selector: 'app-item-details',
   templateUrl: './item-details.component.html',
   styleUrls: ['./item-details.component.css']
 })
-export class ItemDetailsComponent {
+export class ItemDetailsComponent implements OnInit {
+  item: Item;
 
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      const itemId = params['id']; // Assuming you have a route parameter named 'id' for item identification
+      // Fetch item details using the itemId, e.g., make an API call or retrieve from a service
+      // Assign the fetched item details to the 'item' property
+    });
+  }
 }
